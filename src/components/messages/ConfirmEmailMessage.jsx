5c788319e982e0f5b95d8f9092856d24494a32fa
erratbi <yerratbi@gmail.com>
2017-11-12 20:55:23 +0000

    Authorized routes

---
+++ src/components/messages/ConfirmEmailMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+const ConfirmMessage = () => (
+	<Message info>
+		<Message.Header>Please confirm your email address</Message.Header>
+	</Message>
+);
+
+export default ConfirmMessage;
